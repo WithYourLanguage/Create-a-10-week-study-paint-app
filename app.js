6b9b3d786524f457cd48f76d57f09bfea07f06e5
강 <ghang0501@gmail.com>
2023-01-31 10:03:30 +0900

    Day 1~2 챌린지 제출
    바닐라js로 그림 앱 만들기. 코드 챌린지 1~2일차에 정답으로 제출한 코드입니다.

--- app.js
+++ app.js
@@ -1,129 +1,25 @@
-const saveBtn = document.getElementById("save");
-const textInput = document.getElementById("text");
-const fileInput = document.getElementById("file");
-const modeBtn = document.getElementById("mode-btn");
-const destroyBtn = document.getElementById("Destroy-btn");
-const eraserBtn = document.getElementById("eraser-btn");
-const colorOptions = Array.from(
-  document.getElementsByClassName("color-option")
-);
-const color = document.getElementById("color");
-const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-
-const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 800;
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = lineWidth.value;
-ctx.lineCap = "round";
-let isPainting = false;
-let isFilling = false;
 
-function onMove(event) {
-  if (isPainting) {
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
-    return;
-  }
-  ctx.moveTo(event.offsetX, event.offsetY);
-}
-function startPainting() {
-  isPainting = true;
-}
-function cancelPainting() {
-  isPainting = false;
-  ctx.beginPath();
-}
-function onLineWidthChange(event) {
-  ctx.lineWidth = event.target.value;
-}
+ctx.fillRect(210 - 40, 200 - 30, 15, 100);
+ctx.fillRect(350 - 40, 200 - 30, 15, 100);
+ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
 
-function onColorChange(event) {
-  ctx.strokeStyle = event.target.value;
-  ctx.fillStyle = event.target.value;
-}
+ctx.beginPath(); // 이전에 만든 것들과 연결 끊음
+ctx.fillStyle = "green"; // 색을 yellow로 지정
+ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI); // 오른쪽 눈
+ctx.fill();
 
-function onColorClick(event) {
-  const colorValue = event.target.dataset.color;
-  ctx.strokeStyle = colorValue;
-  ctx.fillStyle = colorValue;
-  color.value = colorValue;
-}
+ctx.beginPath(); // 이전에 만든 것들과 연결 끊음
+ctx.fillStyle = "yellow";
+ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI); // 왼쪽 눈
+ctx.fill();
 
-function onModeClick() {
-  if (isFilling) {
-    isFilling = false;
-    modeBtn.innerText = "Fill";
-  } else {
-    isFilling = true;
-    modeBtn.innerText = "Draw";
-  }
-}
-
-function onCanvasClick() {
-  if (isFilling) {
-    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  }
-}
-function onDestroyClick() {
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-}
-function onEraserClick() {
-  ctx.strokeStyle = "white";
-  ctx.lineWidth = 35;
-  isFilling = false;
-  modeBtn.innerText = "Fill";
-}
-function onFileChange(event) {
-  const file = event.target.files[0];
-  const url = URL.createObjectURL(file);
-  const image = new Image();
-  image.src = url;
-  image.onload = function () {
-    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    fileInput.value = null;
-  };
-}
-function onDoubleClick(event) {
-  const text = textInput.value;
-  // if (text !== "") {
-  //   console.log("테스트용 console.log입니당.");
-  //   ctx.save();
-  //   ctx.lineWidth = 1;
-  //   ctx.font = "68px MuseoModerno";
-  //   ctx.fillText(text, event.offsetX, event.offsetY);
-  //   ctx.restore();
-  // }
-  console.log("테스트용 console.log입니당.");
-  ctx.save();
-  ctx.lineWidth = 1;
-  ctx.font = "68px MuseoModerno";
-  ctx.fillText(text, event.offsetX, event.offsetY);
-  ctx.restore();
-}
-function onSaveClick() {
-  const url = canvas.toDataURL();
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "MyDrawing.png";
-  a.click();
-}
-canvas.addEventListener("dblclick", onDoubleClick);
-canvas.addEventListener("mousemove", onMove);
-canvas.addEventListener("mousedown", startPainting);
-canvas.addEventListener("mouseup", cancelPainting);
-canvas.addEventListener("mouseleave", cancelPainting);
-canvas.addEventListener("click", onCanvasClick);
-lineWidth.addEventListener("change", onLineWidthChange);
-color.addEventListener("change", onColorChange);
-
-colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
-
-modeBtn.addEventListener("click", onModeClick);
-destroyBtn.addEventListener("click", onDestroyClick);
-eraserBtn.addEventListener("click", onEraserClick);
-fileInput.addEventListener("change", onFileChange);
-saveBtn.addEventListener("click", onSaveClick);
+ctx.beginPath(); // 이전에 만든 것들과 연결 끊음
+ctx.fillStyle = "red";
+ctx.arc(250, 100, 25, 0, 3); 
+ctx.fill();
